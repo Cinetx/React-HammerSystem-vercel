e5fbec496f301d38727f19b0d912dcfd8a2bdd0c
Denis Vanyukov <39036064+Cinetx@users.noreply.github.com>
2021-09-09 02:53:41 +0400

    Add files via upload

---
+++ static/js/19.63af3f47.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpemilus=this.webpackJsonpemilus||[]).push([[19],{506:function(t,c,e){"use strict";e.r(c);var n=e(2),s=e(0),a=e(42),l=e(101);c.default=function(t){var c=t.match;return Object(n.jsx)(s.Suspense,{fallback:Object(n.jsx)(l.a,{cover:"content"}),children:Object(n.jsxs)(a.d,{children:[Object(n.jsx)(a.a,{exact:!0,from:"".concat(c.url),to:"".concat(c.url,"/clients")}),Object(n.jsx)(a.b,{path:"".concat(c.url,"/clients"),component:Object(s.lazy)((function(){return e.e(18).then(e.bind(null,497))}))})]})})}}}]);
+//# sourceMappingURL=19.63af3f47.chunk.js.map
